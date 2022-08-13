d5ba743bec33d8af4298f67be3d8fbba2e78cefb
amaxxx93293 <110700560+amaxxx93293@users.noreply.github.com>
2022-08-13 22:08:53 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -38,7 +38,7 @@ if (!empty($_SESSION['user'])) {
 ?><!doctype html>
 <html>
 <head>
-	<title>Dashboard | MCHostPanel</title>
+	<title>Dashboard | ACHOST</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/bootstrap-responsive.min.css">
